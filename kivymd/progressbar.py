b38c7d8003957c66bf12748fe342178118892f21
Shan He <shanhex@gmail.com>
2017-11-10 22:15:35 -0800

    add UI changes

--- kivymd/progressbar.py
+++ kivymd/progressbar.py
@@ -18,20 +18,20 @@ Builder.load_string('''
         Color:
             rgba:  self.theme_cls.divider_color
         Rectangle:
-            size:    (self.width*0.6, dp(18)) if self.orientation == 'horizontal' else (dp(18),self.height-dp(8)) 
-            pos:   (self.x+self.width*0.2, self.center_y + dp(54)) if self.orientation == 'horizontal' \
+            size:    (self.width*0.6, dp(18)) if self.orientation == 'horizontal' else (dp(18),self.height-dp(8))
+            pos:   (self.x+self.width*0.2, self.center_y) if self.orientation == 'horizontal' \
                 else (self.center_x + dp(24),self.y)
-        
-            
+
+
         Color:
-            #rgba:  self.theme_cls. primary_color 
-            #rgba:  self.get_color() if self.test else self.theme_cls. primary_color 
+            #rgba:  self.theme_cls. primary_color
+            #rgba:  self.get_color() if self.test else self.theme_cls. primary_color
             rgba:  get_color_from_hex(self.rgbr)
         Rectangle:
             size:     (self.width*self.value_normalized*0.6, sp(18)) if self.orientation == 'horizontal' else (sp(18), \
                 self.height*self.value_normalized)
-                #self.width*(1-self.value_normalized)+self.x      
-            pos:    (self.width*0.6*(1-self.value_normalized)+self.x+self.width*0.2 if self.reversed else self.x+self.width*0.2, self.center_y + dp(54)) \
+                #self.width*(1-self.value_normalized)+self.x
+            pos:    (self.width*0.6*(1-self.value_normalized)+self.x+self.width*0.2 if self.reversed else self.x+self.width*0.2, self.center_y) \
                 if self.orientation == 'horizontal' else \
                 (self.center_x + dp(24),self.height*(1-self.value_normalized)+self.y if self.reversed else self.y)
 ''')
@@ -44,10 +44,10 @@ class MDProgressBar(ThemableBehavior, ProgressBar):
     ''' Orientation of progressbar'''
     rgbr = StringProperty("FF0000")
     test = BooleanProperty(False)
-    #rgbr = OptionProperty('FF0000', 
+    #rgbr = OptionProperty('FF0000',
         #options=['FF0000','00FF00','0000FF'])
     alpha =NumericProperty(.9)
-    
+
 
     def on_rgbr(self,instance,value):
         # self.rgbr = value
@@ -59,7 +59,7 @@ class MDProgressBar(ThemableBehavior, ProgressBar):
         r=int((r1*self.value+r2*(100-self.value))/100)
         g=int((g1*self.value+g2*(100-self.value))/100)
         b=int((b1*self.value+b2*(100-self.value))/100)
-        
+
         self.rgbr= hex(r)[2:].zfill(2)+hex(g)[2:].zfill(2)+hex(b)[2:].zfill(2)
     def on_value(self,instance,value):
         r1,r2=255,153
@@ -68,24 +68,24 @@ class MDProgressBar(ThemableBehavior, ProgressBar):
         r=int((r1*self.value+r2*(100-self.value))/100)
         g=int((g1*self.value+g2*(100-self.value))/100)
         b=int((b1*self.value+b2*(100-self.value))/100)
-        
+
         self.rgbr= hex(r)[2:].zfill(2)+hex(g)[2:].zfill(2)+hex(b)[2:].zfill(2)
-       
+
     # def get_color(self):
     #     color = get_color_from_hex(self.rgbr)
     #     color[3]=self.alpha
     #     print "color"
     #     print color
     #     return color
-        
 
-    
-            
-    
+
+
+
+
 if __name__ == '__main__':
     from kivy.app import App
     from kivymd.theming import ThemeManager
-    
+
     class ProgressBarApp(App):
         theme_cls = ThemeManager()
         rvalue = NumericProperty(40)
@@ -109,7 +109,7 @@ BoxLayout:
         min:0
         max:100
         value: 10
-        
+
     MDProgressBar:
         value: app.rvalue
         rgbr:"00FF00"
@@ -125,7 +125,7 @@ BoxLayout:
             rgbr:"FFFF00"
             test: True
             alpha:.7
-            
+
         MDProgressBar:
             orientation:"vertical"
             value: slider.value
@@ -140,9 +140,9 @@ BoxLayout:
             halign: 'center'
             height: self.texture_size[1] + dp(64)
 
-            
-        
+
+
 """)
-            
+
 
     ProgressBarApp().run()
